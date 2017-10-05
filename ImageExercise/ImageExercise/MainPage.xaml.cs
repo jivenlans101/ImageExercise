8fe01398dc0744c8932165cf44938f667dbfd6b1
Jiven Tabien <jiven.tabien@service101plus.com>
2017-10-05 16:15:46 +0800

    Add project files.

---
+++ ImageExercise/ImageExercise/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        private int imageCount = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_OnClicked(object sender, EventArgs e)
+        {
+            
+            if (imageCount == 1)
+            {
+                imageCount = 10;
+            }
+            else
+            {
+                
+                imageCount--;
+            }
+            Image();
+        }
+        void Button_OnClicked2(object sender, EventArgs e)
+        {
+            if (imageCount == 10)
+            {
+                imageCount = 1;
+            }
+            else
+            {
+                imageCount++;
+            }
+            Image();
+        }
+        void Image()
+        {
+            sampleImage.Source = new UriImageSource
+            {
+                Uri = new Uri("http://lorempixel.com/1920/1080/city/{imageCount}"),
+                CachingEnabled = false
+            };
+        }
+
+
+
+    }
+}
